1952899ec509e1b123661c971b234a39f07c054a
linkeyun <714997079@qq.com>
2021-04-22 23:01:54 +0800

    首页完成

--- src/pages/home/component/Swiper.vue
+++ src/pages/home/component/Swiper.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="wrapper">
-    <swiper ref="mySwiper" :options="swiperOptions">
-      <swiper-slide v-for="item of swiperList" :key="item.id">
+    <swiper ref="mySwiper" :options="swiperOptions" v-if="showSwiper">
+      <swiper-slide v-for="item of list" :key="item.id">
         <img class="swiper-img" :src="item.imgUrl">
       </swiper-slide>
       <div class="swiper-pagination" slot="pagination"></div>
@@ -12,24 +12,23 @@
 <script>
 export default {
   name: 'HomeSwiper',
+  props: {
+    list: Array
+  },
   data () {
     return {
       swiperOptions: {
         pagination: {
           el: '.swiper-pagination'
         },
-        loop: true
-      },
-      swiperList: [{
-        id: '0001',
-        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1801/1a/94428c6dea109402.jpg_640x200_2cf590d8.jpg'
-      }, {
-        id: '0002',
-        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1802/42/7c92b9a381e46402.jpg_640x200_1cdce2a4.jpg'
-      }, {
-        id: '0003',
-        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1802/51/e78f936a5b404102.jpg_640x200_c14f0b3a.jpg'
-      }]
+        loop: true,
+        autoplay: true
+      }
+    }
+  },
+  computed: {
+    showSwiper () {
+      return this.list.length
     }
   }
 }
--- src/pages/home/component/Weekend.vue
+++ src/pages/home/component/Weekend.vue
@@ -6,7 +6,7 @@
     <ul>
       <li
         class="item"
-        v-for="item of recommendList"
+        v-for="item of list"
         :key="item.id">
         <div class="item-img-wrapper">
           <img :src="item.imgUrl" class="item-img">
@@ -23,25 +23,8 @@
 <script>
 export default {
   name: 'HomeWeekend',
-  data () {
-    return {
-      recommendList: [{
-        id: '0001',
-        imgUrl: 'http://img1.qunarzz.com/sight/source/1510/6e/1ea71e2f04e.jpg_r_640x214_aa6f091d.jpg',
-        title: '北京温泉排行榜',
-        desc: '细数北京温泉，温暖你的冬天'
-      }, {
-        id: '0002',
-        imgUrl: 'http://img1.qunarzz.com/sight/source/1510/6e/1ea71e2f04e.jpg_r_640x214_aa6f091d.jpg',
-        title: '北京温泉排行榜',
-        desc: '细数北京温泉，温暖你的冬天'
-      }, {
-        id: '0003',
-        imgUrl: 'http://img1.qunarzz.com/sight/source/1510/6e/1ea71e2f04e.jpg_r_640x214_aa6f091d.jpg',
-        title: '北京温泉排行榜',
-        desc: '细数北京温泉，温暖你的冬天'
-      }]
-    }
+  props: {
+    list: Array
   }
 }
 </script>
@@ -51,7 +34,6 @@ export default {
 .weekend
   background: #eee
   .title
-    margin-top: .2rem
     line-height: .8rem
     background: #eee
     text-indent: .2rem
@@ -61,7 +43,7 @@ export default {
     .item-img-wrapper
       overflow: hidden
       height: 0
-      padding-bottom: 33.9%
+      padding-bottom: 37.09%
       .item-img
         width: 100%
     .item-info
